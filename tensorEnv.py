0ee305a0eeb427c9f1e3d3a9b1dc12531d941928
trojan-A <adityanath11@outlook.com>
2020-08-05 23:57:38 +0530

    Aug_05

--- tensorEnv.py
+++ tensorEnv.py
@@ -1,2 +1,9 @@
-import tensorflow
-import keras
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv("student-mat.csv", sep=";")
+print(data.head())
+data = data[["G1","G2","G3", "studytime", "absences", "failures"]] 
