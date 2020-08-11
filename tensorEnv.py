40b1dd0996d190357cad82b93198a6ae9d899b73
trojan-A <adityanath11@outlook.com>
2020-08-11 23:15:21 +0530

    Aug_9

--- tensorEnv.py
+++ tensorEnv.py
@@ -5,5 +5,15 @@ from sklearn import linear_model
 from sklearn.utils import shuffle
 
 data = pd.read_csv("student-mat.csv", sep=";")
-print(data.head())
+
 data = data[["G1","G2","G3", "studytime", "absences", "failures"]] 
+predict = "G3"
+x = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+
+linear = linear_model.LinearRegression()
+linear.fit(x_train, y_train)
+acc = linear.score(x_test, y_test)
+print(acc)
